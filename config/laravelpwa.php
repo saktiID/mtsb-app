0de57328772c50ed076551b914e3e47601790d75
SaktiID <51077353+saktiID@users.noreply.github.com>
2024-07-17 13:02:49 +0700

    manifest pwa orientation

--- config/laravelpwa.php
+++ config/laravelpwa.php
@@ -103,6 +103,7 @@ return [
                 ],
             ],
             'description' => 'Aplikasi untuk sarana belajar di MTsB',
+            'orientation' => 'portrait',
         ],
     ],
 ];
