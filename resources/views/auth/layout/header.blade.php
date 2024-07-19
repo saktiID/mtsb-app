1eaf5bec23ec65d02e90c799ac9f23e73bd6462c
SaktiID <51077353+saktiID@users.noreply.github.com>
2024-07-19 11:30:09 +0700

    meta first page

--- resources/views/auth/layout/header.blade.php
+++ resources/views/auth/layout/header.blade.php
@@ -2,7 +2,26 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no">
-    <title>@yield('title', 'MTsB App') | MTsB App</title>
+
+    <!-- Primary Meta Tags -->
+    <title>MTs Bilingual APP — Aplikasi sarana belajar</title>
+    <meta name="title" content="MTs Bilingual — Aplikasi sarana belajar" />
+    <meta name="description" content="Dengan aplikasi ini siswa dapat melihat daftar nilai yang dicapai selama pembelajaran di MTs Bilingual Muslimat NU Pucang Sidoarjo." />
+    <meta name="keywords" content="mtsb, app mtsb, mtsb-app, app-mtsb">
+
+    <!-- Open Graph / Facebook -->
+    <meta property="og:type" content="website" />
+    <meta property="og:title" content="MTs Bilingual — Aplikasi sarana belajar" />
+    <meta property="og:description" content="Dengan aplikasi ini siswa dapat melihat daftar nilai yang dicapai selama pembelajaran di MTs Bilingual Muslimat NU Pucang Sidoarjo." />
+    <meta property="og:image" content="https://metatags.io/images/meta-tags.png" />
+
+    <!-- Twitter -->
+    <meta property="twitter:card" content="summary_large_image" />
+    <meta property="twitter:title" content="MTs Bilingual — Aplikasi sarana belajar" />
+    <meta property="twitter:description" content="Dengan aplikasi ini siswa dapat melihat daftar nilai yang dicapai selama pembelajaran di MTs Bilingual Muslimat NU Pucang Sidoarjo." />
+    <meta property="twitter:image" content="https://metatags.io/images/meta-tags.png" />
+
+    {{-- <title>@yield('title', 'MTsB App') | MTsB App</title> --}}
     <link rel="icon" type="image/png" href="{{ asset('logo.png') }}" />
     <!-- BEGIN GLOBAL MANDATORY STYLES -->
     <link href="https://fonts.googleapis.com/css?family=Quicksand:400,500,600,700&display=swap" rel="stylesheet">
