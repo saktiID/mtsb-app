6c222dbd0a4652436060f0e1344f21b4da8cb495
SaktiID <51077353+saktiID@users.noreply.github.com>
2024-08-06 15:06:09 +0700

    testing model13

--- resources/views/admin/agenda/assessment-history/history.blade.php
+++ resources/views/admin/agenda/assessment-history/history.blade.php
@@ -19,8 +19,11 @@
                         <tr>
                             <th width="20%">Nama</th>
                             <td>
+                                @foreach ($siswas as $siswa)
+                                <p>{{ $siswa->user->id }}</p>
+                                @endforeach
+                                <?php die; ?>
 
-                                @dd($siswas)
                                 <select id="siswa_kelas" name="siswa_kelas" class="form-control basic">
                                     <option value="" selected disabled>-- Pilih siswa --</option>
                                     @foreach ($siswas as $siswa)
