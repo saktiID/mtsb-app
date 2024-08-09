5ee2e6787f14a46aeb7f7baaa667bfec64368c1f
SaktiID <51077353+saktiID@users.noreply.github.com>
2024-08-09 19:39:23 +0700

    kelas migration bagian kelas lenght 12

--- database/migrations/2024_07_05_105543_create_kelas_table.php
+++ database/migrations/2024_07_05_105543_create_kelas_table.php
@@ -18,7 +18,7 @@ return new class extends Migration
             $table->uuid('periode_id')->index();
             $table->foreign('periode_id')->references('id')->on('periodes')->onDelete('cascade');
             $table->string('jenjang_kelas', 2);
-            $table->string('bagian_kelas', 2);
+            $table->string('bagian_kelas', 12);
             $table->uuid('walas_id')->nullable()->index();
             $table->foreign('walas_id')->references('id')->on('users')->onDelete('cascade');
             $table->timestamps();
