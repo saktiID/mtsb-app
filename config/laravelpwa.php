27bed9ad705c81c2e18bde8c916594e742480d57
SaktiID <51077353+saktiID@users.noreply.github.com>
2024-08-19 10:06:13 +0700

    manifest icon kemerdekaan

--- config/laravelpwa.php
+++ config/laravelpwa.php
@@ -6,43 +6,43 @@ return [
         'name' => env('APP_NAME', 'My PWA App'),
         'short_name' => 'MTsB',
         'start_url' => '/',
-        'background_color' => '#ffffff',
+        'background_color' => '#EB2C26',
         'theme_color' => '#000000',
         'display_override' => ['fullscreen'],
         'display' => 'standalone',
         'orientation' => 'any',
-        'status_bar' => 'black',
+        'status_bar' => '#EB2C26',
         'icons' => [
             '72x72' => [
-                'path' => '/images/icons/icon-72x72.png',
+                'path' => '/images/icons/kemerdekaan/icon-72x72.png',
                 'purpose' => 'any',
             ],
             '96x96' => [
-                'path' => '/images/icons/icon-96x96.png',
+                'path' => '/images/icons/kemerdekaan/icon-96x96.png',
                 'purpose' => 'any',
             ],
             '128x128' => [
-                'path' => '/images/icons/icon-128x128.png',
+                'path' => '/images/icons/kemerdekaan/icon-128x128.png',
                 'purpose' => 'any',
             ],
             '144x144' => [
-                'path' => '/images/icons/icon-144x144.png',
+                'path' => '/images/icons/kemerdekaan/icon-144x144.png',
                 'purpose' => 'any',
             ],
             '152x152' => [
-                'path' => '/images/icons/icon-152x152.png',
+                'path' => '/images/icons/kemerdekaan/icon-152x152.png',
                 'purpose' => 'any',
             ],
             '192x192' => [
-                'path' => '/images/icons/icon-192x192.png',
+                'path' => '/images/icons/kemerdekaan/icon-192x192.png',
                 'purpose' => 'any',
             ],
             '384x384' => [
-                'path' => '/images/icons/icon-384x384.png',
+                'path' => '/images/icons/kemerdekaan/icon-384x384.png',
                 'purpose' => 'any',
             ],
             '512x512' => [
-                'path' => '/images/icons/icon-512x512.png',
+                'path' => '/images/icons/kemerdekaan/icon-512x512.png',
                 'purpose' => 'any',
             ],
         ],
@@ -64,7 +64,7 @@ return [
                 'description' => 'Shortcut MTsB Application',
                 'url' => '/home',
                 'icons' => [
-                    'src' => '/images/icons/icon-96x96.png',
+                    'src' => '/images/icons/kemerdekaan/icon-96x96.png',
                     'sizes' => '96x96',
                     'purpose' => 'any',
                 ],
@@ -73,14 +73,14 @@ return [
         'custom' => [
             'screenshots' => [
                 [
-                    'src' => '/images/icons/icon-512x512.png',
+                    'src' => '/images/icons/kemerdekaan/icon-512x512.png',
                     'sizes' => '512x512',
                     'type' => 'image/png',
                     'form_factor' => 'wide',
                     'label' => 'MTsB Application',
                 ],
                 [
-                    'src' => '/images/icons/icon-384x384.png',
+                    'src' => '/images/icons/kemerdekaan/icon-384x384.png',
                     'sizes' => '384x384',
                     'type' => 'image/png',
                     'form_factor' => 'narrow',
@@ -94,7 +94,7 @@ return [
                     'url' => '/home',
                     'icons' => [
                         [
-                            'src' => '/images/icons/icon-96x96.png',
+                            'src' => '/images/icons/kemerdekaan/icon-96x96.png',
                             'type' => 'image/png',
                             'sizes' => '96x96',
                             'purpose' => 'any',
