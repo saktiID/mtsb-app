099cc2e2fc4b7cec12b4b8c8f51b0bfbd52ad17d
SaktiID <51077353+saktiID@users.noreply.github.com>
2024-08-26 01:56:14 +0700

    test api wa

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,4 +17,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::post('wa-hook', function (Request $request) {
+    DB::table('api_wa')->insert([
+        'message' => $request->json('message'),
+    ]);
 });
